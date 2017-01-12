08b135bb01d8def2ff157154f94c9424a153d7a9
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-01-12 08:29:49 -0500

    fixed queries in movies

--- skeleton/movie_novice/queries.rb
+++ skeleton/movie_novice/queries.rb
@@ -23,18 +23,26 @@
 
 def find_angelina
   #find Angelina Jolie by name in the actors table
+  Actor.find_by_name("Angelina Jolie")
 
 end
 
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
+  Movie
+    .select(:id, :title)
+    .where("score >= 9")
 
 end
 
 def star_wars
   #display the id, title and year of each Star Wars movie in movies.
   # hint: use 'select' and 'where'
+
+  Movie
+     .select(:id, :title, :yr)
+     .where("title LIKE '%Star Wars%'")
 
 end
 
@@ -45,6 +53,13 @@ def below_average_years
   #in descending order
   # hint: use 'select', 'where', 'group', 'order'
 
+  Movie
+  .select("yr", "COUNT(*) as bad_movies")
+  .where("score < 5")
+  .group("yr")
+  .order("bad_movies DESC")
+
+
 end
 
 def alphabetized_actors
@@ -53,6 +68,9 @@ def alphabetized_actors
   # Note: Ubuntu users may find that special characters
   # are alphabetized differently than the specs.
   # This spec might fail for Ubuntu users. It's ok!
+  Actor
+    .order("name ASC")
+    .limit(10)
 
 end
 
@@ -60,7 +78,10 @@ def pulp_fiction_actors
   # practice using joins
   # display the id and name of all actors in the movie Pulp Fiction
   # hint: use 'select', 'joins', 'where'
-
+  Actor
+    .select(:id, :name)
+    .joins(:movies)
+    .where("title = 'Pulp Fiction'")
 end
 
 def uma_movies
@@ -69,4 +90,9 @@ def uma_movies
   # order them by ascending year
   # hint: use 'select', 'joins', 'where', and 'order'
 
+    Movie
+      .select(:id, :title, :yr)
+      .joins(:actors)
+      .where("name = 'Uma Thurman'")
+      .order("yr ASC")
 end
